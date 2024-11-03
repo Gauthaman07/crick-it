4c4acfe0773da097e72a1943e729186d466a694d
Gautham <gowthaman@ableaura.com>
2024-11-03 09:07:27 +0530

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,10 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  pathPrefix: "/https://github.com/Gauthaman07/crick-it",
+  pathPrefix: "/crick-it",
   siteMetadata: {
     title: `CricKonnect`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://gauthaman07.github.io/crick-it`,
   },
   plugins: ["gatsby-plugin-sass", {
     resolve: 'gatsby-plugin-manifest',
